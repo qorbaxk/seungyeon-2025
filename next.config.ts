a29b768a869db6a4e9e51f62c508f4d20f828eb1
baxksy <qorbaxk@nextinnovation.kr>
2025-01-24 12:27:21 +0900

    fix : 이미지 엑박 오류

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    path: "https://qorbaxk.github.io/seungyeon-2025",
+    loader: "imgix",
+    path: "https://qorbaxk.github.io/seungyeon-2025/",
   },
 };
 
